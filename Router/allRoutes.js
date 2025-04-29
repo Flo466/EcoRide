7b9b3700978d3d951d4be41e405f9901c8c83957
Flo466 <bruel.florent@gmail.com>
2025-04-29 16:40:38 +0200

    fix routing system, import bootstrap and set-up with sass

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "front/pages/home.html"),];
+    new Route("/", "Accueil", "pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
