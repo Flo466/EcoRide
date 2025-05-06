e0acdf04ec08e9bd2a1848a7f2718b370e4cba0a
Flo466 <bruel.florent@gmail.com>
2025-05-06 18:14:53 +0200

    test mongoDB connection

--- backend/sql/structure_database.sql
+++ backend/sql/structure_database.sql
@@ -1,12 +1,9 @@
--- MySQL Script modified for appropriate ON DELETE behavior
--- Cleaned and corrected version
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `sf_ecoride` DEFAULT CHARACTER SET utf8 ;
+USE `sf_ecoride` ;
 
 DROP TABLE IF EXISTS `user_has_role`, `parameter`, `configuration`, `user_has_carpooling`, `carpooling`, `car`, `brand`, `user_has_review`, `review`, `role`, `user`;
 
