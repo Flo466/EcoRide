b74635fc1938a166a6eea11f33fca12f9f246702
Flo466 <bruel.florent@gmail.com>
2025-05-31 16:30:02 +0200

    implement login, acount and edit in sceciruty controller

--- backend/src/Controller/SecurityController.php
+++ backend/src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,7 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 #[Route('/resgistration', name: 'app_api')]
 final class SecurityController extends AbstractController
@@ -43,5 +46,62 @@ final class SecurityController extends AbstractController
             'apiToken' => $user->getApiToken(),
             'roles' => $user->getRoles()],
         status: Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request, UserPasswordHasherInterface $hasher):JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'] ?? null;
+        $password = $data['password'] ?? null;
+
+         if (!$email || !$password) {
+            return new JsonResponse(['message' => 'Email and paswword required'], Response::HTTP_BAD_REQUEST);
+        }
+
+         $user = $this->manager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (!$user || !$hasher->isPasswordValid($user, $password)) {
+            return new JsonResponse(['message' => 'Incorect ID'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return new JsonResponse([
+            'user' => $user->getUserIdentifier(),
+            'apiToken' => $user->getApiToken(),
+            'roles' => $user->getRoles(),
+        ], Response::HTTP_OK);
+    }
+
+    public function account(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return new JsonResponse(['message' => 'missing credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+        
+        return new JsonResponse(
+            json_decode($this->serializer->serialize($user, 'json'), true),
+            Response::HTTP_OK
+        );
+    }
+
+    public function edit(#[CurrentUser] ?User $user, Request $request): JsonResponse
+    {
+        if (null === $user) {
+            return new JsonResponse(['message' => 'missing credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = $this->serializer->deserialize(
+            $request->getContent(),
+            User::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $user]
+        );
+
+        $user->setUpdatedAt(new DateTimeImmutable());
+        $this->manager->flush();
+
+        return new JsonResponse(
+            json_decode($this->serializer->serialize($user, 'json'), true),
+            Response::HTTP_OK
+        );
     }
 }
