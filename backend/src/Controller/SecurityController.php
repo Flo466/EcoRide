92ed67038d9a9f09351c0aaa19d4be3b5ffa1ffa
Flo466 <bruel.florent@gmail.com>
2025-06-04 21:07:07 +0200

    add api documentation security controller

--- backend/src/Controller/SecurityController.php
+++ backend/src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use OpenApi\Attributes as OA;
 use App\Service\TokenService;  // Importation du service TokenService
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -27,7 +28,45 @@ final class SecurityController extends AbstractController
     {
     }
 
+    // Registration Route / API doc
     #[Route('/registration', name: 'registration', methods: ['POST'])]
+    #[OA\Post(
+        path: "/api/registration",
+        summary: "Registering a new user",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "User data required for registration",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "firstName", type: "string", example: "Fisrt name"),
+                    new OA\Property(property: "lastName", type: "string", example: "Last name"),
+                    new OA\Property(property: "userName", type: "string", example: "User name"),
+                    new OA\Property(property: "email", type: "string", example: "adresse@email.com"),
+                    new OA\Property(property: "password", type: "string", example: "password")
+                ],
+                required: ["userName", "email", "password", "firstName", "lastName"],
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "User successfully registered",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "user", type: "string", example: "Nom d'utilisateur"),
+                        new OA\Property(property: "firstName", type: "string", example: "User fisrt name"),
+                        new OA\Property(property: "lastName", type: "string", example: "User last name"),
+                        new OA\Property(property: "apiToken", type: "string", example: "31a023e212f116124a36af14ea0c1c3806eb9378"),
+                        new OA\Property(property: "roles", type: "array", items: new OA\Items(type: "string", example: "ROLE_USER")),
+                    ]
+                )
+            )
+        ]
+    )]
+
+    // Registration function
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $user = $this->serializer->deserialize(
@@ -41,6 +80,7 @@ final class SecurityController extends AbstractController
         $user->setCreatedAt(new \DateTimeImmutable());
 
         $this->tokenService->setApiToken($user);
+        $user->setCredits(20);
 
         $this->manager->persist($user);
         $this->manager->flush();
@@ -50,11 +90,53 @@ final class SecurityController extends AbstractController
             'firstName' => $user->getFirstName(),
             'lastName' => $user->getLastName(),
             'apiToken' => $user->getApiToken(),
+            'credits' => $user->getCredits(),
             'roles' => $user->getRoles()
         ], status: Response::HTTP_CREATED);
     }
 
+    // Login route / API Doc
     #[Route('/login', name: 'login', methods: ['POST'])]
+    #[OA\Post(
+        path: "/api/login",
+        summary: "Logging in a user",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "User credentials for login",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "email", type: "string", example: "adresse@email.com"),
+                    new OA\Property(property: "password", type: "string", example: "Mot de passe")
+                ],
+                required: ["email", "password"]
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "User successfully logged in",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "user", type: "string", example: "Nom d'utilisateur"),
+                        new OA\Property(property: "apiToken", type: "string", example: "31a023e212f116124a36af14ea0c1c3806eb9378"),
+                        new OA\Property(property: "roles", type: "array", items: new OA\Items(type: "string", example: "ROLE_USER")),
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Invalid credentials"
+            ),
+            new OA\Response(
+                response: 400,
+                description: "Missing email or password"
+            )
+        ]
+    )]
+
+    //Login function
     public function login(Request $request, UserPasswordHasherInterface $hasher): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -78,7 +160,40 @@ final class SecurityController extends AbstractController
         ], Response::HTTP_OK);
     }
 
+    // Account/me route / API Doc
     #[Route('/account/me', name: 'me', methods: ['GET'])]
+    #[OA\Get(
+        path: "/api/account/me",
+        summary: "Get user data"
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Data successfully found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "id", type: "integer", example: 1),
+                new OA\Property(property: "firstName", type: "string", example: "first name"),
+                new OA\Property(property: "lastName", type: "string", example: "last name"),
+                new OA\Property(property: "email", type: "string", example: "user@example.com"),
+                new OA\Property(property: "created_at", type: "string", format: "date-time"),
+                new OA\Property(property: "updated_at", type: "string", format: "date-time"),
+                new OA\Property(property: "api_token", type: "string", example: "abcdef1234567890"),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized"
+    )]
+    #[OA\Header(
+        header: "Authorization",
+        description: "Bearer token",
+        required: true,
+        schema: new OA\Schema(type: "string")
+    )]
+
+    // Me function
     public function me(#[CurrentUser] ?User $user): JsonResponse
     {
         if (null === $user) {
@@ -91,7 +206,44 @@ final class SecurityController extends AbstractController
         );
     }
 
+    // Account/edit route / API Doc
     #[Route('/account/edit', name: 'edit', methods: ['PUT'])]
+    #[OA\Put(
+        path: "/api/account/edit",
+        summary: "Edit a user",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "User data required for edition",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "firstName", type: "string", example: "Nouveau prénom")
+                ],
+            )
+        )
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "User updated successfully",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "id", type: "integer", example: 1),
+                new OA\Property(property: "email", type: "string", example: "user@example.com"),
+                new OA\Property(property: "firstName", type: "string", example: "first name"),
+                new OA\Property(property: "lastName", type: "string", example: "last name"),
+                new OA\Property(property: "created_at", type: "string", format: "date-time"),
+                new OA\Property(property: "updated_at", type: "string", format: "date-time"),
+                new OA\Property(property: "api_token", type: "string", example: "abcdef1234567890"),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized"
+    )]
+
+    // Edit function
     public function edit(#[CurrentUser] ?User $user, Request $request): JsonResponse
     {
         if (null === $user) {
