f70328b72ae1153a9eb193d370931f2d4c7322e3
Flo466 <bruel.florent@gmail.com>
2025-07-03 21:30:49 +0200

    create page my-account

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,8 @@ export const allRoutes = [
     new Route("/carpooling", "Covoiturages", "pages/carpooling.html", "js/carpooling.js"),
     new Route("/detail-carpooling", "Détail covoiturage", "pages/detail-carpooling.html", "js/detail-carpooling.js"),
     new Route("/connection", "Compte", "pages/account/connection.html", "js/account/connection.js"),
-    new Route("/create-account", "Créer compte", "pages/account/create-account.html", "js/account/create-account.js")
+    new Route("/create-account", "Créer compte", "pages/account/create-account.html", "js/account/create-account.js"),
+    new Route("/my-account", "Mon compte", "pages/account/my-account.html", "js/account/my-account.js")
 ];
 
 export const websiteName = "EcoRide";
